8dab3c8cf9401adcaf871b6036343f659584cc28
hoodielive <hoodielive@gmail.com>
2018-10-01 15:11:22 -0400

    find some other intro - damn.. sick of hello, world

---
+++ basics/code_struct_02/main.cpp
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *    Description: C++ Tutorials 
+ *
+ *        Version:  1.0
+ *        Created:  10/01/2018 02:57:29 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Larry 
+ *   Organization:  Life 
+ *
+ * =====================================================================================
+ */
+#include <iostream> 
+using namespace std; 
+
+// main() is where program execution begins.
+int main() {
+  cout << "I am sick of Hello World.. damn"; 
+  return 0; 
+}
+
