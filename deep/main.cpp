f63735e4217e82b7e0531d5f4fd1c91d633959d4
hoodielive <hoodielive@gmail.com>
2018-11-24 13:22:46 -0500

    its going to be a great day lol

---
+++ deep/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() 
+{
+  // insert code
+  std::cout << "Hello Aeon\n";
+  return 0; 
+}
---
+++ deep/takeinput.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+
+  int a,b,c;
+  std::cout << "Enter 2 numbers:\n";
+  std::cin >> a >> b;
+  c = a + b;
+  std::cout << "Sum is " << c; 
+  return 0; 
+}
