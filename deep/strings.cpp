c41f2b3090a17ff689645bd43c57ae379afce525
hoodielive <hoodielive@gmail.com>
2018-11-24 13:39:15 -0500

    strings std/stdin

---
+++ deep/strings.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std; 
+
+int main()
+{
+
+  string str;
+
+  cout << "May I know your name sweet girlie?\n";
+
+  cin >> str;
+
+  cout << "Hello " << str;
+
+  return 0;
+}
