8207ae419a8db99a6a960ae7fd96d64fc35be3ad
hoodielive <hoodielive@gmail.com>
2018-11-24 22:02:00 -0500

    Variable literals - A

---
+++ deep/greetings.cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+using namespace std; 
+
+int main() 
+{
+  string str; 
+  cout << "Hi, what is your name?"; 
+  cin >> str; 
+  cout << "Hello, " << str; 
+  return 0; 
+}
---
+++ deep/variable-literals.cpp
@@ -0,0 +1,35 @@
+#include <iostream> 
+using namespace std; 
+
+int main()
+{
+  char a, b, c, d; 
+  float f, g, h;  
+  string i; 
+
+  a = 'A'; 
+  cout << a << endl; 
+
+  b = 65; 
+  cout << b << endl; 
+  
+  c = 0x41; 
+  cout << c << endl; 
+
+  d = 65.5; 
+  cout << d << endl; 
+
+  f = 126.56; 
+  cout << f << endl; 
+
+  g = 123e2; 
+  cout << g  << endl; 
+
+  h = 123e-2; 
+  cout << h << endl; 
+
+  i = "Hello"; 
+  cout << i << endl; 
+
+  return 0; 
+}
