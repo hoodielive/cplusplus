9bf8cbb90cf6b91b8cce9f30dde20ac03ab23d29
hoodielive <hoodielive@gmail.com>
2018-11-24 22:15:46 -0500

    with errors - d(10) cannot be used as a function && g{10} token error

---
+++ deep/ten.cpp
@@ -0,0 +1,30 @@
+#include <iostream> 
+using namespace std; 
+
+int main() 
+{
+  int a, b, c, d, f, g, h; 
+
+  a = 10; 
+  cout << a << endl; 
+
+  b = 010; 
+  cout << b << endl; 
+
+  c = 0x10; 
+  cout << c << endl; 
+
+  d(10); 
+  cout << d << endl; 
+
+  f = (10); 
+  cout << f << endl; 
+
+  g{10}; 
+  cout << g << endl; 
+
+  h = {10}; 
+  cout << h << endl; 
+
+  return 0; 
+}
