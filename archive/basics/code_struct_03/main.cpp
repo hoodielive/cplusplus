fb283dd4afd1dcb03c351e617dd44f5066613cea
hoodielive <hoodielive@gmail.com>
2019-06-05 18:36:56 -0400

    lets create strings

--- archive/basics/code_struct_03/main.cpp
+++ archive/basics/code_struct_03/main.cpp
@@ -9,4 +9,6 @@ int main()
     cin >> a >> b;
     c = a + b;
     cout << "Sum is " << c << endl;
+
+    return 0; 
 }
---
+++ archive/basics/code_struct_03/string.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string str;
+    cout << "May I know your name please? "; 
+    cin >> str; 
+    cout << "Hello " << str; 
+    return 0;
+}
