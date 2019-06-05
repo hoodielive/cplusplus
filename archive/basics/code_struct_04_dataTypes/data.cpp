d7cb14285f9baf13694d94d8059667595a3ea7d3
hoodielive <hoodielive@gmail.com>
2019-06-05 19:58:36 -0400

    ...

---
+++ archive/basics/code_struct_04_dataTypes/data.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std; 
+
+int main()
+{
+  long f; 
+  long double d; 
+  cout << sizeof(f) << endl;
+  cout << CHAR_MIN << " " << CHAR_MAX << endl;
+  return 0;
+}
---
+++ archive/basics/code_struct_05_variables/variables.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+
+using namespace std; 
+
+int main()
+{
+  int marks; 
+  int days(1);
+  int stuffs=(1);
+  marks = 10; 
+  cout << marks << endl; 
+  cout << stuffs << endl; 
+  cout << days << endl; 
+  return 0; 
+}
