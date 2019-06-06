5b15a01f3425675e352e904b1ebc0a090470535d
hoodielive <hoodielive@gmail.com>
2019-06-06 19:57:42 -0400

    area radius

---
+++ archive/basics/code_struct_06_operators_expressions/area.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+
+using namespace std; 
+
+int main()
+{
+  float area, r; 
+  cout << "Enter Radius " << endl; 
+  cin >> r; 
+  area = 3.1425f * r * r; 
+  cout << "Your area radius is " << area << endl; 
+  return 0;
+}
