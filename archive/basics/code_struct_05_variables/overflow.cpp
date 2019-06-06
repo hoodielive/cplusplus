c62973c87a60fd4758c88bd2636f18a653d6c0cb
hoodielive <hoodielive@gmail.com>
2019-06-06 19:13:05 -0400

    INT_MAX

---
+++ archive/basics/code_struct_05_variables/overflow.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+
+using namespace std; 
+
+int main()
+{
+  char x = 127; 
+  int inta = INT_MAX; 
+  x++;
+  x++;
+  cout << (int)x << endl;
+  cout << (int)inta << endl;
+  return 0; 
+}
