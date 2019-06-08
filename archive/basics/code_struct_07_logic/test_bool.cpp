22faf2158c30df291cfd464989a5465c69d1198c
hoodielive <hoodielive@gmail.com>
2019-06-08 18:55:55 -0400

    negation bool

---
+++ archive/basics/code_struct_07_logic/test_bool.cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+
+using namespace std; 
+
+int main()
+{
+  if ( ! true && !false )
+  {
+    cout << "You are the messenger of truth! " << endl;  
+  }
+  else 
+  {
+    cout << "You are the messenger of falsehoods " << endl;
+  }
+
+  return 0; 
+}
