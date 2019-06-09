98b8cd106783c36e28f66771e76ac7c1487502bc
hoodielive <hoodielive@gmail.com>
2019-06-09 19:03:59 -0400

    blah..

---
+++ archive/basics/code_struct_11_loops/while.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+
+using namespace std; 
+
+int main()
+{
+	while ( true ) 
+	{
+		cout "input or output: " << endl; 
+		cin >> input; 
+		if ( ! isValid ( input ) )
+		{
+			continue; 
+		}
+}
