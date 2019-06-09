458f819da03ede7616de16f4ff74b4780eaa86f0
hoodielive <hoodielive@gmail.com>
2019-06-09 16:30:20 -0400

    write validator

---
+++ archive/basics/code_struct_10_conditions/check.cpp
@@ -0,0 +1,20 @@
+#include <iostream> 
+
+using namespace std; 
+
+int main()
+{
+	int a, b, c; 
+	cout << "Enter 2 numbers: " << endl; 
+	cin >> a >> b; 
+	if (b == 0) 
+	{
+		cout << "Division by Zero" << endl; 
+	}
+	else 
+	{
+		c = a / b; 
+		cout << c << endl; 
+	}
+	return 0; 
+}
