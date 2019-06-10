1a855fbb65ec235e43423be4ee985704c5a51ebf
hoodielive <hoodielive@gmail.com>
2019-06-10 17:55:29 -0400

    arrays

--- archive/basics/code_struct_12_arrays/arrays.cpp
+++ archive/basics/code_struct_12_arrays/arrays.cpp
@@ -8,10 +8,14 @@ int main()
 {
  	int A[5] = {2, 5, 8, 10, 16}; 
 	string B[3] = { "This", "is", "string" };
+	int C[]={2,4,6,8,10,12}; 
 
 	for ( int i = 0; i < 3; i++)
 	{
 		cout << B[i] << endl; 
 	}
+
+//	for (int x : C)
+//		cout << x << endl; 
 	return 0; 
 }
