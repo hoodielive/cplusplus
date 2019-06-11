9b884b2a49268b846af4edb76f09d81b3012660e
hoodielive <hoodielive@gmail.com>
2019-06-11 19:10:57 -0400

    string physics lol

---
+++ archive/basics/code_struct_13_strings/strings.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std; 
+
+int main()
+{
+	string h = "Hello";
+	string w ("World"); 
+	cout << h << " " << w << endl; 
+	
+	// String Combining 
+	string a = h + " " + w; 
+	cout << a << endl; 
+
+	h += " " + w; 
+	cout << h << endl; 
+
+	string b = "Hello" + w; 
+	cout << b << endl; 
+}
