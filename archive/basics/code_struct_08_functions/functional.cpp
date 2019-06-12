affe289d1108b8424ea550336a449976bab7c219
hoodielive <hoodielive@gmail.com>
2019-06-11 21:33:12 -0400

    ase

---
+++ archive/basics/code_struct_08_functions/functional.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+#include <string> 
+using namespace std; 
+
+void myFunction(string a, string b)
+{
+	cout << a + " " + b << endl; 
+}
+
+int main()
+{
+	myFunction("Howdy", "Aeon"); 
+	return 0; 
+}
