283accafdd3db35061c0c9b8c18aa55a0f5c3b09
hoodielive <hoodielive@gmail.com>
2019-06-11 20:09:26 -0400

    string functions

--- archive/basics/code_struct_13_strings/string_functions.cpp
+++ archive/basics/code_struct_13_strings/string_functions.cpp
@@ -6,5 +6,11 @@ using namespace std;
 int main()
 {
 
+	string s = "Hello"; 
+	size_t i = s.length(); // 5, length of string 
+	cout << i << endl; 
+	
+	int r = s.size(); 
+	cout << r << endl; 
 	return 0; 
 }
