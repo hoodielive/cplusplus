3a7d5b101df52414eaac0f271aee857cac0e757d
hoodielive <hoodielive@gmail.com>
2019-06-12 21:23:11 -0400

    correct foreach

--- archive/basics/code_struct_12_arrays/foreach.cpp
+++ archive/basics/code_struct_12_arrays/foreach.cpp
@@ -5,7 +5,7 @@ int main()
 	int B[6]={2,3,4};
 	for (int x: B)
 	{
-		std::cout << "These are the elems from the B array: " << B[] << std::endl; 
+		std::cout << "This element is: " << x << std::endl; 
 	}
 	return 0;
 }
