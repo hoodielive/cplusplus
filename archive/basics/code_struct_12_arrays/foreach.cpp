2d40c9457246ef190fa3fbc6ec412efd740fc6d7
hoodielive <hoodielive@gmail.com>
2019-06-12 21:08:35 -0400

    fix this crap

---
+++ archive/basics/code_struct_12_arrays/foreach.cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+
+int main() 
+{
+	int B[6]={2,3,4};
+	for (int x: B)
+	{
+		std::cout << "These are the elems from the B array: " << B[] << std::endl; 
+	}
+	return 0;
+}
---
+++ archive/basics/code_struct_12_arrays/loop_thru_arrays.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+
+using namespace std; 
+
+int main() 
+// fix this shit. 
+{
+	int numElems; 
+
+	for (int i = numElems - 1; i >= 0; i--) 
+	{
+		cout << array[i]
+	}
+	return 0; 
+}
