f74ff5f6fe093b8ff45de2e91f72323d374ce600
hoodielive <hoodielive@gmail.com>
2019-06-12 21:24:37 -0400

    on that note..

--- archive/basics/code_struct_12_arrays/foreach.cpp
+++ archive/basics/code_struct_12_arrays/foreach.cpp
@@ -3,9 +3,16 @@
 int main() 
 {
 	int B[6]={2,3,4};
+	int C[]={2,3,4,5,6,7,8,9}; 
+
 	for (int x: B)
 	{
 		std::cout << "This element is: " << x << std::endl; 
+	}
+
+	for (int c: C)
+	{
+		std::cout << "This element is: " << c << std::endl; 
 	}
 	return 0;
 }
