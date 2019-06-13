d0db67b2e09c53adbfd76237d8df453bf45e8d40
hoodielive <hoodielive@gmail.com>
2019-06-12 21:36:56 -0400

    string arrays

--- archive/basics/code_struct_12_arrays/foreach.cpp
+++ archive/basics/code_struct_12_arrays/foreach.cpp
@@ -1,18 +1,24 @@
 #include <iostream> 
+#include <string> 
+using std::string; 
 
 int main() 
 {
 	int B[6]={2,3,4};
 	float C[]={2.5f,3.2f,4.9f,5.0f,6,7,8,9}; 
+	string str[]={"love", "patience", "forgiveness", "friendship"};
 
 	for (int x: B)
 	{
 		std::cout << "This element is: " << x << std::endl; 
 	}
 
-	for (int c: C)
+	for (float c: C)
 	{
 		std::cout << "This element is: " << c << std::endl; 
 	}
+
+	for (auto d: str)
+		std::cout << "We should aspire toward: " << d << std::endl; 
 	return 0;
 }
