3f89998b174514e0a7adfcb26dee2385038e1870
hoodielive <hoodielive@gmail.com>
2019-07-04 08:52:35 -0400

    revist arrays

---
+++ archive/basics/code_struct_12_arrays/revist.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    int example[5]; 
+    example[0] = 2; 
+    example[4] = 4;
+
+    std::cout << example[0] << std::endl; 
+    std::cout << example << std::endl; 
+
+    std::cin.get();
+
+}
