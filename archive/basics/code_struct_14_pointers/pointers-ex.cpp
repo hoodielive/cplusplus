1529a854cc80ee08003e15a1b63bad67c7a05ce0
hoodielive <hoodielive@gmail.com>
2019-07-25 21:10:46 -0400

    pointers

--- archive/basics/code_struct_14_pointers/pointers-ex.cpp
+++ archive/basics/code_struct_14_pointers/pointers-ex.cpp
@@ -11,6 +11,14 @@ int main()
 
   std::cout << "Address of i: " << p << std::endl;
   std::cout << "Value of i: " << *p << std::endl;
+  
+  int* p2 = p; 
+  std::cout << "Copy of p in p2: " << p2 << std::endl;
+ 
+  // sometimes we may want to point to a pointer 
+  int** r = &p; // pointer to p (assigns address of p)
 
+  std::cout << "Address of p: " << r << std::endl;
+  std::cout << "Address of i: " << *r << std::endl;
   return 0;
 }
