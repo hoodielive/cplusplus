764afb72abb822bec90228c38825f6451f3a04ce
hoodielive <hoodielive@gmail.com>
2019-07-29 20:12:09 -0400

    tupleology

---
+++ archive/basics/code_struct_21_tuples/tuples-and-stuff.cpp
@@ -0,0 +1,18 @@
+#include <iostream> 
+#include <tuple> 
+
+int main() 
+{
+    auto first = make_tuple(10,  'A'); 
+
+    const int maxN = 1e9; 
+    const int maxL = 15; 
+
+    auto second = make_tuple(maxN, maxL)
+
+    cout << get<0>(first) << " " << get<1>(first) << "\n"; 
+    cout << get<0>(second) << " " << get<1>(second) << "\n"; 
+
+    return 0;
+
+}
