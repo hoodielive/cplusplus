41199f1defad0a0d9624534fe1e667725435d16c
hoodielive <hoodielive@gmail.com>
2019-09-08 12:07:27 -0400

    setup

---
+++ moderncplusplus/DefinesType/user-defined-types.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() 
+{
+    /** tell the class where the file is by making the compiler a promise 
+     * linker will ensure it is kept
+     * #include only a convenience 
+     * put each class in its own file
+    */ 
+
+   
+
+
+
+}
