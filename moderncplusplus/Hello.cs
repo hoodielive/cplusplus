824e4f4d43e3e7a04d36ea87f83403abb29c1957
hoodielive <hoodielive@gmail.com>
2019-09-07 21:41:16 -0400

    sigh..

---
+++ moderncplusplus/Hello.cs
@@ -0,0 +1,13 @@
+using System.Collections; 
+using System.Collections.Generic; 
+using System; 
+
+public class Hello
+{
+	public static string name = "Vincent"; 
+
+	public static void Main(string[] args)
+	{
+		Console.WriteLine("Hello, " + name); 
+	}
+}
