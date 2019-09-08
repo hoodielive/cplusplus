61ed748b4c9773d8603e790e65cdbbec9629c3ff
hoodielive <hoodielive@gmail.com>
2019-09-07 22:12:02 -0400

    variables

---
+++ moderncplusplus/variables.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+#include <string> 
+
+using namespace std; 
+
+
+int main() 
+{
+    string _name = "Larry"; 
+    cout << "Hello, you can call me " << _name << endl; 
+    
+    return 0; 
+} 
