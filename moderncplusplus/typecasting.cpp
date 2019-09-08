f9747054718154704c33b9674586d94c91e7b8b3
hoodielive <hoodielive@gmail.com>
2019-09-08 12:01:47 -0400

    casting is dangerous

--- moderncplusplus/typecasting.cpp
+++ moderncplusplus/typecasting.cpp
@@ -3,5 +3,12 @@
 int main() 
 {
     // casting can backfire! 
-    
+    int i1 = 3; 
+    double d1 = 2.2; 
+    int d2 = i1; 
+    int i5 = static_cast<int>(d1); 
+    std::cout << "This is the d1= " << d1 << std::endl;
+    std::cout << "This is the i5 cast scenario: " << i5 << std::endl; 
+
+    return 0;
 }
