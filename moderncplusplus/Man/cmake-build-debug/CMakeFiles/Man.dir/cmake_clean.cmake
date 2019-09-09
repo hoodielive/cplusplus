ecd70e16365a93bc27da73d05246c36fa5cc9c4d
hoodielive <hoodielive@gmail.com>
2019-09-08 23:20:13 -0400

    dope c++

---
+++ moderncplusplus/Man/cmake-build-debug/CMakeFiles/Man.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Man.dir/main.cpp.o"
+  "CMakeFiles/Man.dir/Man.cpp.o"
+  "Man.pdb"
+  "Man"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Man.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
