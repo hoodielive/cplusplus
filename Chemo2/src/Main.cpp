703331920a8eb001cf19f95aa4aa745ec25ab670
hoodielive <hoodielive@gmail.com>
2019-09-09 22:18:57 -0400

    I add cout to see if this code would run properly

--- Chemo2/src/Main.cpp
+++ Chemo2/src/Main.cpp
@@ -13,6 +13,9 @@ void Move(Player& player, int xa, int ya)
 {
     player.x += xa * player.speed;
     player.y += ya * player.speed;
+
+    std::cout << "The return for player.x is: " << player.x  << std::endl;
+    std::cout << "The return for player.y is: " << player.y  << std::endl;
 }
 
 int main()
@@ -20,6 +23,7 @@ int main()
    Player player;
 
    player.x = 5;
+   player.y = 15;
 
    Move(player, 1, -1);
 
