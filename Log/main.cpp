40c54440fdff490ac5b1db8b4753ba860e36ed78
hoodielive <hoodielive@gmail.com>
2019-09-10 19:25:57 -0400

    I created methods to facilitate the logging for Error, Warn and Info

--- Log/main.cpp
+++ Log/main.cpp
@@ -3,21 +3,41 @@
 class Log
 {
 public:
-    void SetLevel(int Level)
-    {
+    const int LogLevelWarn = 1;
+    const int LogLevelError = 0;
+    const int LogLevelInfo = 2;
 
+private:
+    int m_LogLevel = LogLevelInfo;
+
+public:
+    void SetLevel(int level)
+    {
+        m_LogLevel = level;
+    }
+    void Error(const char* message)
+    {
+        if (m_LogLevel >= LogLevelError)
+            std::cout << "[ERROR]: " << message << std::endl;
     }
 
     void Warn(const char* message)
     {
+        if (m_LogLevel >= LogLevelWarn)
+            std::cout << "[WARNING]: " << message << std::endl;
+    }
 
+    void Info(const char* message)
+    {
+        if (m_LogLevel >= LogLevelInfo)
+            std::cout << "[INFO]: " << message << std::endl;
     }
 };
 
 int main()
 {
     Log log;
-    log.SetLevel(LogLevelWarning);
+    log.SetLevel(log.LogLevelWarn);
     log.Warn("Warning!!!");
     std::cin.get();
 }
