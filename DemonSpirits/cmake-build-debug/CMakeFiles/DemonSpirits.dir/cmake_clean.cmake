8a8ee58ff839af2a2afd6a36ce2106d78138c565
hoodielive <hoodielive@gmail.com>
2019-09-10 20:52:43 -0400

    demons..

---
+++ DemonSpirits/cmake-build-debug/CMakeFiles/DemonSpirits.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DemonSpirits.dir/main.cpp.o"
+  "DemonSpirits.pdb"
+  "DemonSpirits"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DemonSpirits.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ DemonSpirits/cmake-build-debug/CMakeFiles/DemonSpirits.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for DemonSpirits.
+# This may be replaced when dependencies are built.
