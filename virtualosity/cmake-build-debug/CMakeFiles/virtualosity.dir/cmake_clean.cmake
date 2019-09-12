f9ce744e2284575b3f02fb61abfa1c6c4fd58bbc
hoodielive <hoodielive@gmail.com>
2019-09-12 19:21:12 -0400

    virtual functions

---
+++ virtualosity/cmake-build-debug/CMakeFiles/virtualosity.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/virtualosity.dir/main.cpp.o"
+  "virtualosity.pdb"
+  "virtualosity"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/virtualosity.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
