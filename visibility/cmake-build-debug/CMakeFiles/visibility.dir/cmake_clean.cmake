75588ebb04f068ee304663163898bc0513f9c3a2
hoodielive <hoodielive@gmail.com>
2019-09-15 01:19:45 -0400

    updated gitignore

---
+++ visibility/cmake-build-debug/CMakeFiles/visibility.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/visibility.dir/main.cpp.o"
+  "visibility.pdb"
+  "visibility"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/visibility.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ visibility/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+class Entity
+{
+    int X, Y;
+};
+
+
+
+
+int main()
+{
+
+
+}
+
