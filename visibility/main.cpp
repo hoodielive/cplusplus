10811775fe13102ad85643b349ad7e470a9de7d2
hoodielive <hoodielive@gmail.com>
2019-09-16 22:02:48 -0400

    added proper syntax - I forgot to include << lol

--- visibility/main.cpp
+++ visibility/main.cpp
@@ -2,10 +2,24 @@
 
 int main()
 {
-   int card[5];
+    int array[8][8]; // Declares an array that looks like a chess board
 
-   for (int i = 0; i < 5; i++)
-   {
-       card[i] = getRandomCard();
-   }
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+           array[i][j] = i * j;
+        }
+    }
+
+    std::cout << "Multiplication table: " << std::endl;
+
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            std::cout << "[ "<< i <<" ][ "<< j <<" ] = " << std::endl;
+            std::cout << array[i][j] <<" " << std::endl;
+        }
+    }
 }
