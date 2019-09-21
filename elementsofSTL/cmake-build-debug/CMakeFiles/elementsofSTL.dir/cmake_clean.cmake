ce31d755df42b7041d4f70ab70f52dfbd3431949
hoodielive <hoodielive@gmail.com>
2019-09-21 13:44:12 -0400

    wrote some notes

---
+++ elementsofSTL/cmake-build-debug/CMakeFiles/elementsofSTL.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/elementsofSTL.dir/main.cpp.o"
+  "elementsofSTL.pdb"
+  "elementsofSTL"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/elementsofSTL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
