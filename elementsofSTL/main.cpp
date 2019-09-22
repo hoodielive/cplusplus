30baf160a7facf2cdfbafaf40ee3255321473626
hoodielive <hoodielive@gmail.com>
2019-09-22 08:58:19 -0400

    worked on STL features, however, I think I'll move to some more essentials today - this should have been committed yesterday but I started watching movies

--- elementsofSTL/main.cpp
+++ elementsofSTL/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -9,10 +10,31 @@ int size_in_bits(const T& a)
     return sizeof(a)*8;
 }
 
-int main()
+void Print(const vector<int>& my_vector)
 {
+    cout << "Vector content: { ";
+    for(const int& num : my_vector)
+        cout << num << " ";
+    cout << " }\n\n";
+}
+
+int main() {
     cout << size_in_bits(21) << endl;
     cout << size_in_bits('f') << endl;
     cout << size_in_bits(32.1f) << endl;
     cout << size_in_bits(32.1) << endl;
+
+    vector<int> numbers;
+    int value = 0;
+
+    cout << "Pushing back..\n";
+    while(value >=0)
+    {
+        cout << "Enter number: ";
+        cin >> value;
+        if (value >= 0)
+            numbers.push_back(value);
+    }
+
+    Print(numbers);
 }
