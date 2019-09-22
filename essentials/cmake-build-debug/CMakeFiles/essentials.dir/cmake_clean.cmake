9baa392ba2f374b11df2212831f8a1e60385f1e5
hoodielive <hoodielive@gmail.com>
2019-09-22 09:28:41 -0400

    set up for essentials

---
+++ essentials/cmake-build-debug/CMakeFiles/essentials.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/essentials.dir/main.cpp.o"
+  "essentials.pdb"
+  "essentials"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/essentials.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ essentials/cmake-build-debug/CMakeFiles/essentials.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for essentials.
+# This may be replaced when dependencies are built.
