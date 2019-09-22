5451d7f04f6b5b6795c5095577f962541f27e66d
hoodielive <hoodielive@gmail.com>
2019-09-22 10:38:59 -0400

    just playing around

--- essentialc/main.c
+++ essentialc/main.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 
-int main(int argc, char ** argv)
+int fun()
 {
-    printf("Um... what's up yo?");
+    for (int i = 1; i <= 5; ++i)
+        printf("i is %d\n", i);
+    return 0;
+};
+
+int code()
+{
+    int i = 7;
+    char* s = "tells on everybody";
+    printf("Egun Tupac %s\n", s);
     return 0;
 }
 
+int main(int argc, char ** argv)
+{
+    int x = printf("This is something!") * 5;
+    printf("Um... what's up yo?\n");
+    printf("printf returned %d\n", x);
+
+    fun();
+    code();
+    return 0;
+}
