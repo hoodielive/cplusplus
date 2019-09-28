bc89d4825766a7e15b11ad3c2a36ac1790dec391
hoodielive <hoodielive@gmail.com>
2019-09-27 22:39:36 -0400

    this some bullshit

---
+++ notmychild/cmake-build-debug/CMakeFiles/notmychild.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/notmychild.dir/main.cpp.o"
+  "notmychild.pdb"
+  "notmychild"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/notmychild.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ notmychild/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+class Mac()
+{
+    int findSmallestRemainingElement(int array[], int index)
+    {
+        int index_of_smallest_value = index;
+        for(int i = index + 1; i < sizeof(array); i++)
+        {
+            if (array[i] < array[index_of_smallest_value])
+                index_of_smallest_value = I;
+        }
+    }
+    return index_of_smallest_value;
+
+    int main()
+    {
+        Mac m = new Mac();
+        int w[4] {3, 2, 1, 0, 4};
+        m.findSmallestRemainingElement(w, 2);
+        return 0;
+    }
+}
+
