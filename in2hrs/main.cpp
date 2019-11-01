585731c604c46ff2e4edff2b10b93f25dcdc118f
hoodielive <hoodielive@gmail.com>
2019-11-01 17:15:16 -0400

    c++ gods

--- in2hrs/main.cpp
+++ in2hrs/main.cpp
@@ -7,7 +7,21 @@
 #include <numeric>
 #include <cmath>
 
+int imGlobal = 0;
+const double PI = 3.14;
+bool married = true;
+char myGrade = 'A';
+short int smallNum = 10;
+
 int main(int argc, char** argv)
 {
+	float f1 = 1.1111111;
+	float f2 = 1.1111111;
+
+	printf("Sum = %.7f\n", (f1 + f2));
+	
+	auto whatAmI = true; 
+
 	std::cout << "Hello" << std::endl;
+	std::cout << "Min Double" << std::numeric_limits<double>::min() << std::endl;
 }
