592d543223f2d2823ad7dd1712e4261ff8666174
hoodielive <hoodielive@gmail.com>
2019-12-14 17:12:22 -0500

    I feel so lazy.. LOL

---
+++ games/basics/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	constexpr WORLD_LENGTH= 5;
+	std::cout << "Welcome to Bulls and Cows" << std::endl;
+	std::cout << "Can you guess the X letter isogram I'm thinking of?\n";
+
+	return 0;
+}
