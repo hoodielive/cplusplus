f9e6b79b75f094237a8cd03ce95ce346f6f201e0
hoodielive <hoodielive@gmail.com>
2019-12-17 19:31:33 -0500

    tree

--- Tree/tree_01/mapper.cpp
+++ Tree/tree_01/mapper.cpp
@@ -3,11 +3,11 @@
 
 int main()
 {
-	std::map<std::string, int> mapUsersAge {{"Alex", 45}, {"John", 25 }};
+	std::map<std::string, int> mapUsersAge {{"Alex", 45}, {"Larry", 25 }};
 	
 	std::map mapCopy{mapUsersAge};
 
-	if (auto [iter, wasAdded] = mapCopy.insert_or_assign("John", 26); !wasAdded)
+	if (auto [iter, wasAdded] = mapCopy.insert_or_assign("Larry", 26); !wasAdded)
 		std::cout << iter->first << " reassigned..\n";
 
 	for (const auto& [key, value] : mapCopy)
